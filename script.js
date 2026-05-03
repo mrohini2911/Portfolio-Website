4d31e99b8339f08d9cab5cee663c6e13f259694a
mrohini2911 <mrohini2911@gmail.com>
2026-05-03 16:15:39 +0530

    script.js

--- script.js
+++ script.js
@@ -1,9 +1,29 @@
+// Smooth scroll
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function(e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
+
+// Skill animation
+const bars = document.querySelectorAll(".bar div");
+
+window.addEventListener("scroll", () => {
+  bars.forEach(bar => {
+    bar.style.width = bar.getAttribute("style").split(":")[1];
+  });
+});
+
+// Particles background
 particlesJS("particles-js", {
   particles: {
     number: { value: 80 },
     size: { value: 3 },
     move: { speed: 2 },
-    line_linked: { enable: true },
+    line_linked: { enable: true }
   },
   interactivity: {
     events: {
